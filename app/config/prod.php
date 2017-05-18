0676a91df5cdb72c26c85945d9508abfa8490537
Your Actual Name <Your Actual Email>
2017-05-18 15:29:58 +0200

    htaccess changed

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,6 +1,5 @@
 <?php
 
 // configure your app for the production environment
-
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
